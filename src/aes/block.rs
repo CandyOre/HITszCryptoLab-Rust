0edb3614a115d1f7690d815bb331b3e4e8dc8392
CandyOreWin10 <834657460@qq.com>
2022-09-27 19:41:15 +0800

    aes encrypt

--- src/aes/block.rs
+++ src/aes/block.rs
@@ -1,6 +1,8 @@
-use super::row::Row;
+use std::num::Wrapping;
 
-#[derive(Clone, Default)]
+use super::{row::Row, consts::SBOX};
+
+#[derive(Clone, Default, Copy)]
 pub struct Block {
     content: [[u8; 4]; 4],
 }
@@ -21,6 +23,70 @@ impl Block {
             Some(block)
         }
     }
+
+    pub fn from_column_iter<T, E>(src: T) -> Option<Block>
+    where
+        T: IntoIterator<Item = E>,
+        E: num_traits::AsPrimitive<u8>,
+    {
+        let mut sz = 0 as usize;
+        let mut content: [u8; 16] = Default::default();
+        for e in src.into_iter() {
+            content[sz as usize] = e.as_();
+            sz += 1;
+        }
+        if sz == 16 {
+            Some(Self::from(content).transpose())
+        } else {
+            None
+        }
+    }
+
+    pub fn transpose(&mut self) -> Self {
+        for i in 0..4 {
+            for j in 0..i {
+                (self.content[i][j], self.content[j][i])
+                    = (self.content[j][i], self.content[i][j]);
+            }
+        }
+        *self
+    }
+
+    pub fn into_iter(&self) -> impl Iterator<Item = u8> {
+        self.content.into_iter().flat_map(|x| x.into_iter())
+    }
+
+    pub fn into_column_iter(&self) -> impl Iterator<Item = u8> {
+        self.clone().transpose().into_iter()
+    }
+
+    pub fn sub(&mut self) -> Self {
+        Self::from_column_iter(
+            self.into_column_iter().map(|x| SBOX[x as usize])
+        ).unwrap()
+    }
+
+    pub fn shift(&mut self) -> Self {
+        self.content[1] = [
+            self.content[1][1],
+            self.content[1][2],
+            self.content[1][3],
+            self.content[1][0],
+        ];
+        self.content[2] = [
+            self.content[2][2],
+            self.content[2][3],
+            self.content[2][0],
+            self.content[2][1],
+        ];
+        self.content[3] = [
+            self.content[3][3],
+            self.content[3][0],
+            self.content[3][1],
+            self.content[3][2],
+        ];
+        *self
+    }
 }
 
 impl From<[u8; 16]> for Block {
@@ -29,6 +95,57 @@ impl From<[u8; 16]> for Block {
         for i in 0..4 {
             for j in 0..4 {
                 block.content[i][j] = e[i*4+j];
+            }
+        }
+        block
+    }
+}
+
+impl std::ops::BitXor<Block> for Block {
+    type Output = Block;
+
+    fn bitxor(self, rhs: Block) -> Self::Output {
+        let mut block: Block = Default::default();
+        for i in 0..4 {
+            for j in 0..4 {
+                block.content[i][j] = self.content[i][j] ^ rhs.content[i][j];
+            }
+        }
+        block
+    }
+}
+
+fn galois_field_mul(a: u8, b: u8) -> u8 {
+    let mut res = 0;
+    let mut a = a;
+    let mut b = b;
+    while a > 0 {
+        if a & 1u8 > 0 {
+            res ^= b;
+        }
+        a >>= 1;
+        let overflow = b & 0x80u8;
+        b <<= 1;
+        if overflow > 0 {
+            b ^= 0x1b;
+        }
+    }
+    res
+}
+
+impl std::ops::Mul<Block> for Block {
+    type Output = Block;
+
+    fn mul(self, rhs: Block) -> Self::Output {
+        let mut block: Block = Default::default();
+        for i in 0..4 {
+            for j in 0..4 {
+                for k in 0..4 {
+                    block.content[i][j] ^= galois_field_mul(
+                        self.content[i][k],
+                        rhs.content[k][j]
+                    );
+                }
             }
         }
         block
--- src/aes/process.rs
+++ src/aes/process.rs
@@ -1,7 +1,32 @@
+use std::ops::Mul;
+
+use crate::console::io::IOHelper;
+
 use super::round_key::RoundKey;
+use super::block::Block;
+use super::consts::{COLM, COLM_INV};
 
 pub fn encrypt(round_key: &RoundKey, plain: &String) -> Vec<u8> {
-    let mut enc = Vec::new();
-    unimplemented!();
+    let plain = aligned(&plain);
+    let mut enc = Vec::with_capacity(plain.len());
+    for i in (0..plain.len()).step_by(16) {
+        let mut plain_block = Block::from_column_iter(
+            plain[i..i+16].to_string().chars()
+        ).unwrap() ^ round_key.k[0];
+        for j in 1..10 {
+            plain_block = COLM.mul(plain_block.sub().shift()) ^ round_key.k[j];
+        }
+        plain_block = plain_block.sub().shift() ^ round_key.k[10];
+        for e in plain_block.into_column_iter() {
+            enc.push(e);
+        }
+    }
     enc
+}
+
+fn aligned(plain: &String) -> String {
+    let mut plain = plain.clone();
+    let left = (16 - plain.len() % 16) % 16;
+    plain.push_str(&"x".repeat(left));
+    plain
 }
