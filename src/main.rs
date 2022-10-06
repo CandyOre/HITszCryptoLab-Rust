4e65dc4d7e9efb61ebdb94ace40f6c66a3ed57e2
CandyOreWin10 <834657460@qq.com>
2022-10-06 18:16:12 +0800

    uniform demo entrance

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,30 @@ mod cypher;
 mod aes;
 mod rsa;
 
+use console::io::IOHelper;
 use demo::Demo;
-#[allow(unused_imports)]
 use aes::Aes;
-#[allow(unused_imports)]
 use rsa::Rsa;
 
 fn main() {
-    let mut demo = Aes::new();
-    demo.start_demo();
+    loop {
+        match IOHelper::get_string_loop(
+            0,
+            "\n".to_string()
+            + "Enter a number to choose demo\n"
+            + "  1 - AES\n"
+            + "  2 - RSA (Default)\n"
+            + "Your choice is: "
+        ).parse::<usize>().unwrap_or(2) {
+            1 => Aes::new().start_demo(),
+            2
+            |_ => Rsa::new().start_demo(),
+        };
+        if IOHelper::get_string_loop(
+            0,
+            "\nEnter 1 to choose another demo: ".to_string()
+        ) != "1" {
+            break;
+        }
+    }
 }
